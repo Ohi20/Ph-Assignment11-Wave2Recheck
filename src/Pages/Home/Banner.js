c59d8635c3d9618caadef976ecb73b46be20694b
Ohi20 <ashrafohi20@gmail.com>
2022-08-02 17:14:59 +0600

    first commit

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-                       <div class="hero min-h-screen text-white font-bold">
-  <div class="hero-content text-center">
-    <div class="max-w-md bg-neutral rounded-3xl px-10 py-5 rounded-5">
-    <h1 class="text-5xl font-bold">Hello There!</h1>
+    
+<div class="hero min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://wallpaperaccess.com/full/33110.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Hello There!</h1>
       <p class="py-6 text-2xl">Track cars you’ve watched.<br></br>
 Fix those you want to see.<br></br>
 Tell us what’s good for you.</p>
-      <button class="btn btn-outline text-white">Get Started</button>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
